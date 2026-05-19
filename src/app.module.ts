979760b3d6d794502eb3dd8cc93445aee5217bcf
hund555 <allanholmiversen@gmail.com>
2026-05-19 10:25:11 +0200

    socket works, need to change to fit project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebsocketGateway } from './websocket/websocket.gateway';
+import { GpsModule } from './gps/gps.module';
+import { AlarmModule } from './alarm/alarm.module';
 
 @Module({
-  imports: [],
+  //imports: [GpsModule, AlarmModule],
   controllers: [AppController],
-  providers: [AppService, WebsocketGateway],
+  providers: [AppService, WebsocketGateway, Logger],
 })
 export class AppModule {}
--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,9 +1,39 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, WebSocket } from 'ws';
 
-@WebSocketGateway()
-export class WebsocketGateway {
+@WebSocketGateway(5000)
+export class WebsocketGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server!: Server;
+
+  constructor(private readonly logger: Logger) {}
+
+  afterInit(server: Server) {
+    this.logger.log('WebSocket server initialized');
+  }
+
+  handleConnection(client: WebSocket) {
+    this.logger.log('Client connected');
+    client.send('Welcome to the WebSocket server!');
+  }
+
+  handleDisconnect(client: WebSocket) {
+    this.logger.log('Client disconnected');
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+    this.logger.log(`Received message: ${payload}`);
+    return `Echo: ${payload}`;
   }
 }
