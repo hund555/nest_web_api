4b345ea9eddab906bd685245c779eab9ecbd2fc6
hund555 <allanholmiversen@gmail.com>
2026-05-19 12:06:47 +0200

    Added database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,36 @@ import { AlarmModule } from './alarm/alarm.module';
 import { TrackersModule } from './trackers/trackers.module';
 import { ResidentsModule } from './residents/residents.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tracker } from './Entities/tracker.entity';
+import { GpsLocation } from './Entities/gps.entity';
+import { Resident } from './Entities/resident.entity';
+import { AlarmLog } from './Entities/alarm.entity';
+import { User } from './Entities/user.entity';
+import { Role } from './Entities/role.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mssql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [Tracker, GpsLocation, Resident, AlarmLog, User, Role],
+        synchronize: true,
+        options: {
+          encrypt: true,
+          trustServerCertificate: true,
+        },
+      }),
+      inject: [ConfigService],
+    }),
     GpsModule, 
     AlarmModule, 
     TrackersModule, 
