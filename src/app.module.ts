223575f92471f1343e9d5254546e008def2f5234
hund555 <allanholmiversen@gmail.com>
2026-05-19 11:29:10 +0200

    Added trackers module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { WebsocketGateway } from './websocket/websocket.gateway';
 import { GpsModule } from './gps/gps.module';
 import { AlarmModule } from './alarm/alarm.module';
+import { TrackersModule } from './trackers/trackers.module';
+import { ResidentsModule } from './residents/residents.module';
 
 @Module({
-  //imports: [GpsModule, AlarmModule],
+  imports: [GpsModule, AlarmModule, TrackersModule, ResidentsModule],
   controllers: [AppController],
   providers: [AppService, WebsocketGateway, Logger],
 })
