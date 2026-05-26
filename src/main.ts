1be2e457b4c95aaf5f87166a58c52a27df9122bc
hund555 <allanholmiversen@gmail.com>
2026-05-26 10:08:56 +0200

    Tracker service interval

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
+  const trackersService =
+    app.get(TrackersService);
+  setInterval(async () => {
+    await trackersService.updateOfflineTrackers();
+    console.log('Checked tracker statuses');
+  }, 30000);// Check every 30 seconds
 }
 bootstrap();
